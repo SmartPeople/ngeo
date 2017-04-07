26e23be40d0f7b085199a24322df808c774ddfc1
smartpeople <semeyon@speople.pro>
2017-04-07 11:12:28 +0600

    Main screen

--- js/components/home/geomainscreen.js
+++ js/components/home/geomainscreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, List, ListItem, Icon } from 'native-base';
 import { Col, Row, Grid } from "react-native-easy-grid";
+import styles from './styles';
 
 function round(ts) {
     return Math.round(ts * 100) / 100;
@@ -49,29 +50,39 @@ export class GeoMainScreen extends Component {
             posArr  = this.state.positionArray;
         return (
             <Grid>
-                <Row>
-                    <Col size={20}><Icon active name="map" style={{textAlign:"right"}} /></Col>
+                <Row style={styles.row}>
+                    <Col size={20}><Icon active name="map" style={styles.icon} /></Col>
                     <Col size={80}>
-                        <Text> Long:{ lastPos ? lastPos.coords.longitude : '' }</Text>
-                        <Text> Lat:{lastPos ? lastPos.coords.latitude : '' } </Text>
-                        <Text> Altitude: {lastPos ? lastPos.coords.altitude : '' } ({lastPos ? lastPos.coords.altitudeAccuracy : '' }) </Text>
-                        <Text> Accuracy:{ lastPos ? lastPos.coords.accuracy : '' }</Text>
+                        <Text style={styles.label}> Latitude</Text>
+                        <Text style={styles.param}> {lastPos ? lastPos.coords.latitude : '' }&#176;</Text>
+                        <Text style={styles.label}> Longitude</Text>
+                        <Text style={styles.param}> { lastPos ? lastPos.coords.longitude : '' }&#176;</Text>
+                        <Text style={styles.label}> Altitude</Text>
+                        <Text style={styles.param}> {lastPos ? lastPos.coords.altitude : '' } m ({lastPos ? lastPos.coords.altitudeAccuracy : '' }m) </Text>
+                        <Text style={styles.label}> Accuracy:</Text>
+                        <Text style={styles.param}> { lastPos ? lastPos.coords.accuracy : '' } git m</Text>
                     </Col>
                 </Row>
-                <Row>
-                    <Col size={20}><Icon active name="speedometer" style={{textAlign:"right"}}  /></Col>
-                    <Col size={80}><Text> Speed:{lastPos ? lastPos.coords.speed : '' } | Avg | lates</Text></Col>
-
-                </Row>
-                <Row>
-                    <Col size={20}><Icon active name="time"  style={{textAlign:"right"}}  /></Col>
+                <Row style={styles.row}>
+                    <Col size={20}>
+                        <Icon active name="speedometer" style={styles.icon} />
+                    </Col>
                     <Col size={80}>
-                        <Row>
-                            <Text> { new Date().toISOString() }</Text>
-                        </Row>
-                        <Row>
-                            <Text> { lastPos ? lastPos.timestamp : '' }</Text>
-                        </Row>
+                        <Text style={styles.label}> Speed:</Text>
+                        <Text style={styles.param}> {lastPos ? lastPos.coords.speed : '' } m/s <Text style={styles.smallNote}>(GEO)</Text></Text>
+                        <Text style={styles.param}> {lastPos ? lastPos.coords.speed : '' } m/s <Text style={styles.smallNote}>(AVG)</Text></Text>
+                        <Text style={styles.param}> {lastPos ? lastPos.coords.speed : '' } m/s <Text style={styles.smallNote}>(LATEST)</Text></Text>
+                    </Col>
+                </Row>
+                <Row style={styles.row}>
+                    <Col size={20}>
+                        <Icon active name="time" style={styles.icon} />
+                    </Col>
+                    <Col size={80}>
+                        <Text style={styles.label}> DateTime:</Text>
+                        <Text style={styles.param}> { new Date().toISOString() }</Text>
+                        <Text style={styles.label}> Timestamp:</Text>
+                        <Text style={styles.param}> { lastPos ? lastPos.timestamp : '' }</Text>
                     </Col>
                 </Row>
             </Grid>
--- js/components/home/styles.js
+++ js/components/home/styles.js
@@ -4,7 +4,7 @@ export default {
   },
   row: {
     flex: 1,
-    alignItems: 'center',
+    marginTop: 20
   },
   text: {
     fontSize: 20,
@@ -14,4 +14,20 @@ export default {
   mt: {
     marginTop: 18,
   },
+  label: {
+    marginTop: 10,
+    fontStyle: 'italic'
+  },
+  icon: {
+    marginTop: 10,
+    textAlign:'center'
+  },
+  param:{
+    fontSize: 22,
+    color: '#0080ff'
+  },
+  smallNote: {
+    fontSize: 10,
+    color: '#808080'
+  }
 };
