48beadaa543a921812248f075fc38f07bb600c59
smartpeople <semeyon@speople.pro>
2017-04-07 14:45:25 +0600

    Small code cleanups

--- js/components/home/screens/geolocationlogs.js
+++ js/components/home/screens/geolocationlogs.js
@@ -5,27 +5,27 @@ import { round, extractLast4 } from '../mathutils';
 
 
 function geoLine(p) {
-    return (
-        <Text>
-            ..{extractLast4(p.timestamp)}&nbsp;
-            ..{extractLast4(p.coords.latitude)}&#176;&nbsp;
-            ..{extractLast4(p.coords.longitude)}&#176;&nbsp;
-            {round(p.coords.altitude)}m&nbsp;
-            {round(p.coords.accuracy)}m&nbsp;
-            {round(p.coords.speed)}m/s
-        </Text>
-    )
+  return (
+    <Text>
+      ..{extractLast4(p.timestamp)}&nbsp;
+      ..{extractLast4(p.coords.latitude)}&#176;&nbsp;
+      ..{extractLast4(p.coords.longitude)}&#176;&nbsp;
+      {round(p.coords.altitude)}m&nbsp;
+      {round(p.coords.accuracy)}m&nbsp;
+      {round(p.coords.speed)}m/s
+    </Text>
+  )
 }
 
 function errorLine(p) {
-    const msg = p.message.split(100);
-    return (
-        <Text>{msg}...</Text>
-    )
+  const msg = p.message.split(100);
+  return (
+    <Text>{msg}...</Text>
+  )
 }
 
 function line(p) {
-    return p.type === POSITION_MSG ? geoLine(p) : errorLine(p);
+  return p.type === POSITION_MSG ? geoLine(p) : errorLine(p);
 }
 
 function AlertJson(p) {
