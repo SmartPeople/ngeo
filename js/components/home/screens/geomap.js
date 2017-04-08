00bba44885a4257bc09b17a5451be42d6c3fb86d
smartpeople <semeyon@speople.pro>
2017-04-08 13:30:03 +0600

    Simple markers

--- js/components/home/screens/geomap.js
+++ js/components/home/screens/geomap.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Dimensions from 'Dimensions';
 
 import MapView from 'react-native-maps';
+import { POSITION_MSG } from '../index';
+
 
 export class GeoMap extends Component {
 
@@ -11,7 +13,9 @@ export class GeoMap extends Component {
     }
 
     render() {
-        const lastPos = this.props.lastPosition;
+        const lastPos = this.props.lastPosition,
+               posArr = this.props.positionArray.filter((p) => p.type === POSITION_MSG);
+
         return (
             <MapView
                 style={{height: Dimensions.get('window').height, width: Dimensions.get('window').width}}
@@ -21,7 +25,15 @@ export class GeoMap extends Component {
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
-            />
+            >
+                {posArr.map(marker => (
+                    <MapView.Marker key={marker.timestamp}
+                        coordinate={marker.coords}
+                        title={marker.timestamp.toString()}
+                        description={marker.coords.speed.toString()}
+                    />
+                ))}
+            </MapView>
         );
     }
 
